572f17bd07038fd235db09e719dadd2a0e091d58
Tejas_27 <tejassharma_ee20a15@dtu.ac.in>
2021-09-13 01:49:00 +0530

    Added the carousal

---
+++ script.js
@@ -0,0 +1,38 @@
+// $('.owl-carousel').owlCarousel({
+//     autoplay: true,
+//     autoplayTimeout: 5000,
+//     autoplayHoverPause: true,
+//     loop: true,
+//     margin: 50,
+//     responsiveClass: true,
+//     nav: true,
+//     loop: true,
+//     stagePadding: 100,
+//     responsive: {
+//       0: {
+//         items: 1
+//       },
+//       568: {
+//         items: 2
+//       },
+//       600: {
+//         items: 3
+//       },
+//       1000: {
+//         items: 3
+//       }
+//     }
+//   })
+//   $(document).ready(function() {
+//     $('.popup-youtube').magnificPopup({
+//       disableOn: 320,
+//       type: 'iframe',
+//       mainClass: 'mfp-fade',
+//       removalDelay: 160,
+//       preloader: false,
+//       fixedContentPos: true
+//     });
+//   });
+//   $('.item').magnificPopup({
+//     delegate: 'a',
+//   });
